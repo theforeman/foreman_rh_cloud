17a04055b551006e70f305e12c2a4cb75ff76f0e
laviro <1ronlavi@gmail.com>
2019-07-11 12:12:22 +0300

    Fix terminal scroll

--- webpack/ForemanYupana/Components/Terminal/Terminal.js
+++ webpack/ForemanYupana/Components/Terminal/Terminal.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'patternfly-react';
+import { Grid, Spinner } from 'patternfly-react';
 import './terminal.scss';
 
-const Terminal = ({ children }) => {
-  const logs =
-    children && children.map((log, index) => <p key={index}>{log}</p>);
-  return (
-    <Grid.Col sm={8}>
-      <div className="terminal">
-        <Grid fluid>
-          <Grid.Row>
-            <Grid.Col sm={12}>{logs}</Grid.Col>
-          </Grid.Row>
-        </Grid>
-      </div>
-    </Grid.Col>
-  );
-};
+class Terminal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.terminal = React.createRef();
+  }
+  componentDidUpdate() {
+    const element = this.terminal.current;
+    element.scrollTop = element.scrollHeight;
+  }
+  render() {
+    const { children } = this.props;
+    const logs =
+      children && children.map((log, index) => <p key={index}>{log}</p>);
+    return (
+      <Grid.Col sm={8}>
+        <div className="terminal" ref={this.terminal}>
+          <Grid fluid>
+            <Grid.Row>
+              <Grid.Col sm={12}>
+                {logs}
+                <Spinner loading inverse inline size="xs" />
+              </Grid.Col>
+            </Grid.Row>
+          </Grid>
+        </div>
+      </Grid.Col>
+    );
+  }
+}
 
 Terminal.propTypes = {
   children: PropTypes.node,
