ba4023840b9157a0351f2dff91331c2ed2ab22f0
laviro <1ronlavi@gmail.com>
2019-07-16 16:30:40 +0300

    remove redux from UploadsDashboard

--- webpack/ForemanYupana/Components/UploadsDashboard/UploadsDashboardSelectors.js
+++
@@ -1,6 +0,0 @@
-import { selectForemanYupana } from '../../ForemanYupanaSelectors';
-
-export const selectUploadsDashboard = state =>
-  selectForemanYupana(state).uploadsDashboard;
-
-export const selectBool = state => selectUploadsDashboard(state).bool;
--- webpack/ForemanYupana/Components/UploadsDashboard/__tests__/UploadsDashboardActions.test.js
+++
@@ -1,9 +0,0 @@
-import { testActionSnapshotWithFixtures } from 'react-redux-test-utils';
-import { changeBool } from '../UploadsDashboardActions';
-
-const fixtures = {
-  'should changeBool': () => changeBool({ bool: true }),
-};
-
-describe('UploadsDashboard actions', () =>
-  testActionSnapshotWithFixtures(fixtures));
