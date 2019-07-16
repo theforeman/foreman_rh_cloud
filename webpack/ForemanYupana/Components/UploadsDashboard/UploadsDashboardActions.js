c3060b5437defa47e9cb8bfb9e6ca1ab20fa5cef
laviro <1ronlavi@gmail.com>
2019-07-16 16:30:40 +0300

    remove redux from UploadsDashboard

--- webpack/ForemanYupana/Components/UploadsDashboard/UploadsDashboardActions.js
+++
@@ -1,8 +0,0 @@
-import { UPLOADSDASHBOARD_CHANGE_BOOL } from './UploadsDashboardConstants';
-
-export const changeBool = resource => dispatch => {
-  dispatch({
-    type: UPLOADSDASHBOARD_CHANGE_BOOL,
-    payload: resource,
-  });
-};
