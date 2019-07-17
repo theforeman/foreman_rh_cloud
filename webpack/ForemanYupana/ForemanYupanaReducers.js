0f36bad44be9620e587541f442d670ffcaf6021e
laviro <1ronlavi@gmail.com>
2019-07-17 10:35:21 +0300

    Rename UploadsDashboard to Dashboard

--- webpack/ForemanYupana/ForemanYupanaReducers.js
+++ webpack/ForemanYupana/ForemanYupanaReducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducers as uploadsDashboardReducers } from './Components/UploadsDashboard';
 import { reducers as reportGenerateReducers } from './Components/ReportGenerate';
 
 const reducers = {
   ForemanYupana: combineReducers({
-    ...uploadsDashboardReducers,
     ...reportGenerateReducers,
   }),
 };
