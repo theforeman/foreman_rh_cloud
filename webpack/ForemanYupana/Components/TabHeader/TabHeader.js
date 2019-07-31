03d1c8ae84512a3527e6953536838e6b885bb2f0
laviro <1ronlavi@gmail.com>
2019-07-31 19:03:35 +0300

    Move download button to the Tab Header

--- webpack/ForemanYupana/Components/TabHeader/TabHeader.js
+++ webpack/ForemanYupana/Components/TabHeader/TabHeader.js
@@ -1,29 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { noop, Grid, Button } from 'patternfly-react';
+import { noop, Grid, Button, ButtonGroup, Icon } from 'patternfly-react';
 import './tabHeader.scss';
 
-const TabHeader = ({ exitCode, onRestart }) => (
-  <Grid.Row className="tab-header">
-    <Grid.Col sm={8}>
-      <h1># Exit Code: {exitCode}</h1>
-    </Grid.Col>
-    <Grid.Col sm={4}>
-      <Button bsStyle="primary" onClick={onRestart}>
-        Restart
+const TabHeader = ({ exitCode, onRestart, onDownload }) => {
+  const restartButton = (
+    <Button bsStyle="primary" onClick={onRestart}>
+      Restart
+    </Button>
+  );
+  const buttons = onDownload ? (
+    <ButtonGroup>
+      {restartButton}
+      <Button onClick={onDownload}>
+        Download Report <Icon name="download" />
       </Button>
-    </Grid.Col>
-  </Grid.Row>
-);
+    </ButtonGroup>
+  ) : (
+    restartButton
+  );
+  return (
+    <Grid.Row className="tab-header">
+      <Grid.Col sm={7}>
+        <p># Exit Code: {exitCode}</p>
+      </Grid.Col>
+      <Grid.Col sm={5}>{buttons}</Grid.Col>
+    </Grid.Row>
+  );
+};
 
 TabHeader.propTypes = {
   onRestart: PropTypes.func,
+  onDownload: PropTypes.func,
   exitCode: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 TabHeader.defaultProps = {
   onRestart: noop,
   exitCode: '',
+  onDownload: null,
 };
 
 export default TabHeader;
