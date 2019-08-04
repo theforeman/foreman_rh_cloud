4ccf5268ce83e2cebebac48577eed98f9b875a37
laviro <1ronlavi@gmail.com>
2019-08-04 13:22:29 +0300

    Replace the EmptyState icon by spinner

--- webpack/ForemanYupana/Components/AccountList/Components/EmptyState/EmptyState.js
+++ webpack/ForemanYupana/Components/AccountList/Components/EmptyState/EmptyState.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { EmptyState } from 'patternfly-react';
+import { EmptyState, Spinner } from 'patternfly-react';
+import './emptyState.scss';
 
 const yupanaEmptyState = () => (
   <EmptyState>
-    <EmptyState.Icon />
+    <Spinner loading inline size="lg" />
     <EmptyState.Title>Fetching data about your accounts</EmptyState.Title>
     <EmptyState.Info>Loading...</EmptyState.Info>
   </EmptyState>
