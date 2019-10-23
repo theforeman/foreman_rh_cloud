58fa7fa8974cbd371b31ed71db56dc0e88fec4c9
Shimon Shtein <sshtein@redhat.com>
2019-10-23 20:36:23 +0300

    Fix tests 1

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -6,20 +6,19 @@ class StatusesControllerTest < ActionController::TestCase
   include FolderIsolation
 
   test 'Returns statuses for each process type' do
-    configuration = RedhatAccess::TelemetryConfiguration.new(enable_telemetry: true, portal_user: 'test')
-    configuration.save!
+    test_org = FactoryBot.create(:organization)
 
-    generate_label = ForemanInventoryUpload::Async::GenerateReportJob.output_label('test')
+    generate_label = ForemanInventoryUpload::Async::GenerateReportJob.output_label(test_org.id)
     generate_output = ForemanInventoryUpload::Async::ProgressOutput.register(generate_label)
     generate_output.status = 'generate_status_test'
-    upload_label = ForemanInventoryUpload::Async::UploadReportJob.output_label('test')
+    upload_label = ForemanInventoryUpload::Async::UploadReportJob.output_label(test_org.id)
     upload_output = ForemanInventoryUpload::Async::ProgressOutput.register(upload_label)
     upload_output.status = 'upload_status_test'
 
     get :index, session: set_session_user
 
     assert_response :success
-    actual = JSON.parse(response.body)['statuses']['test']
+    actual = JSON.parse(response.body)['statuses'][test_org.name]
     assert_equal 'generate_status_test', actual['generate_report_status']
     assert_equal 'upload_status_test', actual['upload_report_status']
   end
