b35f553e1626acc79eb997c042250e0d2decd144
Shimon Shtein <sshtein@redhat.com>
2019-11-03 13:53:31 +0200

    Make inventory upload URL flexible

--- lib/foreman_inventory_upload.rb
+++ lib/foreman_inventory_upload.rb
@@ -42,7 +42,7 @@ module ForemanInventoryUpload
   end
 
   def self.upload_url
-    # for testing set ENV to 'https://ci.cloud.paas.psi.redhat.com/api/ingress/v1/upload'
+    # for testing set ENV to 'https://ci.cloud.redhat.com/api/ingress/v1/upload'
     @upload_url ||= ENV['SATELLITE_INVENTORY_UPLOAD_URL'] || 'https://cloud.redhat.com/api/ingress/v1/upload'
   end
 
