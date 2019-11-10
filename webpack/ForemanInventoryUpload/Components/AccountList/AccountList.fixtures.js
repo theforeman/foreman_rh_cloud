956108375acf671cc070ca9c4ca5d7c76dafcb88
Shimon Shtein <sshtein@redhat.com>
2019-11-10 12:48:33 +0200

    Add labels to API_SUCCESS_RESPONSE

--- webpack/ForemanInventoryUpload/Components/AccountList/AccountList.fixtures.js
+++ webpack/ForemanInventoryUpload/Components/AccountList/AccountList.fixtures.js
@@ -2,14 +2,17 @@ import { noop } from 'patternfly-react';
 
 export const API_SUCCESS_RESPONSE = {
   Account1: {
+    label: 'test_org1',
     upload_report_status: 'running',
     generate_report_status: 'running',
   },
   Account2: {
+    label: 'test_org2',
     upload_report_status: 'unknown',
     generate_report_status: 'failure',
   },
   Account3: {
+    label: 'test_org3',
     upload_report_status: 'success',
     generate_report_status: 'running',
   },
