8f4513fa2c4d1058c9c4881b09242e9dfc542b23
Shimon Shtein <sshtein@redhat.com>
2019-11-17 14:32:27 +0200

    Make eslint happy

--- webpack/ForemanInventoryUpload/Components/AccountList/Components/ListItemStatus/ListItemStatus.fixtures.js
+++ webpack/ForemanInventoryUpload/Components/AccountList/Components/ListItemStatus/ListItemStatus.fixtures.js
@@ -2,6 +2,6 @@ export const props = {
   account: {
     generate_report_status: 'Exit Code: pid 22191 exit 0',
     upload_report_status: 'Exit Code: Running in pid 22376',
-    label: 'Default Org'
+    label: 'Default Org',
   },
 };
