3f8c90d16efa54a00a91a8ce1c052da83a0e9dec
Shimon Shtein <sshtein@redhat.com>
2020-05-05 17:34:43 +0300

    Add cloud identification fields

--- lib/foreman_inventory_upload/generators/fact_helpers.rb
+++ lib/foreman_inventory_upload/generators/fact_helpers.rb
@@ -3,6 +3,11 @@ module ForemanInventoryUpload
     module FactHelpers
       extend ActiveSupport::Concern
 
+      CLOUD_AMAZON = 'aws'
+      CLOUD_GOOGLE = 'google'
+      CLOUD_AZURE = 'azure'
+      CLOUD_ALIBABA = 'alibaba'
+
       def fact_value(host, fact_name)
         value_record = host.fact_values.find do |fact_value|
           fact_value.fact_name_id == ForemanInventoryUpload::Generators::Queries.fact_names[fact_name]
@@ -25,6 +30,26 @@ module ForemanInventoryUpload
 
         @organization_golden_tickets ||= {}
         @organization_golden_tickets[organization.id] ||= result
+      end
+
+      def cloud_provider(host)
+        bios_version = fact_value(host, 'dmi::bios::version')
+
+        if bios_version
+          return CLOUD_AMAZON if bios_version.downcase['amazon']
+          return CLOUD_GOOGLE if bios_version.downcase['google']
+        end
+
+        chassis_asset_tag = fact_value(host, 'dmi::chassis::asset_tag')
+        return CLOUD_AZURE if chassis_asset_tag && chassis_asset_tag['7783-7084-3265-9085-8269-3286-77']
+
+        system_manufacturer = fact_value(host, 'dmi::system::manufacturer')
+        return CLOUD_ALIBABA if system_manufacturer && system_manufacturer.downcase['alibaba cloud']
+
+        product_name = fact_value(host, 'dmi::system::product_name')
+        return CLOUD_ALIBABA if product_name && product_name.downcase['alibaba cloud ecs']
+
+        nil
       end
     end
   end
