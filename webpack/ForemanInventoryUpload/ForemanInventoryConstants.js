a51108ab6c383efc320b55136bf0dc478fc6377e
Shimon Shtein <sshtein@redhat.com>
2020-05-13 10:50:19 +0300

    Make inventoryBaseUrl dynamic

--- webpack/ForemanInventoryUpload/ForemanInventoryConstants.js
+++ webpack/ForemanInventoryUpload/ForemanInventoryConstants.js
@@ -1,5 +1,3 @@
 import { translate as __ } from 'foremanReact/common/I18n';
-import { foremanUrl } from '../ForemanRhCloudHelpers';
 
 export const INVENTORY_PAGE_TITLE = __('Red Hat Inventory Uploads');
-export const INVENTORY_BASE_URL = foremanUrl('/foreman_inventory_upload/');
