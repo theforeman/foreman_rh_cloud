c7d3741743d005c157eabc3787ae6a10c77f3d1b
laviro <1ronlavi@gmail.com>
2020-05-17 17:26:55 +0000

    downgrade

--- script/travis_run_js_tests.sh
+++ script/travis_run_js_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-if [[ $( git diff --name-only origin/master..HEAD webpack/ .travis.yml babel.config.js .eslintrc package.json | wc -l ) -ne 0 ]]; then
+if [[ $( git diff --name-only origin/master..HEAD webpack/ .travis.yml .babelrc .eslintrc package.json | wc -l ) -ne 0 ]]; then
   npm run test;
-  npm run publish-coverage;
+  npm run coveralls;
   npm run lint;
 fi
