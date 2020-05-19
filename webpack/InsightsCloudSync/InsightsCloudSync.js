6c878a9d46594e2aeb131886a9880a61bccdf1d8
Shimon Shtein <sshtein@redhat.com>
2020-05-19 16:07:05 +0300

    Review fixes

--- webpack/InsightsCloudSync/InsightsCloudSync.js
+++ webpack/InsightsCloudSync/InsightsCloudSync.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { IntlProvider } from 'react-intl';
+import PropTypes from 'prop-types';
 import { translate as __ } from 'foremanReact/common/I18n';
-import { Button } from 'patternfly-react';
-import { syncInsights } from './InsightsCloudSyncActions';
+import { Button, noop } from 'patternfly-react';
 
-const InsightsCloudSync = () => {
-  const handleSync = () => {
-    syncInsights();
-  };
+const InsightsCloudSync = ({ syncInsights }) => (
+  <IntlProvider locale={navigator.language}>
+    <div className="insights-cloud-sync">
+      <h1>{__('Red Hat Insights Sync')}</h1>
+      <Button bsStyle="primary" onClick={syncInsights}>
+        {__('Sync now')}
+      </Button>
+    </div>
+  </IntlProvider>
+);
 
-  return (
-    <IntlProvider locale={navigator.language}>
-      <div className="insights-cloud-sync">
-        <h1>{__('Red Hat Insights Sync')}</h1>
-        <Button bsStyle="primary" onClick={handleSync}>
-          {__('Sync now')}
-        </Button>
-      </div>
-    </IntlProvider>
-  );
+InsightsCloudSync.propTypes = {
+  syncInsights: PropTypes.func,
+};
+
+InsightsCloudSync.defaultProps = {
+  syncInsights: noop,
 };
 
 export default InsightsCloudSync;
--- webpack/InsightsCloudSync/InsightsCloudSyncActions.js
+++ webpack/InsightsCloudSync/InsightsCloudSyncActions.js
@@ -2,10 +2,19 @@ import API from 'foremanReact/API';
 import { insightsCloudUrl } from './InsightsCloudSyncHelpers';
 import { INSIGHTS_CLOUD_SYNC_TASK } from './InsightsCloudSyncConstants';
 
-export const syncInsights = () => {
-  API.post(insightsCloudUrl('tasks'));
-  return {
-    type: INSIGHTS_CLOUD_SYNC_TASK,
-    payload: {},
-  };
+export const syncInsights = () => async dispatch => {
+  try {
+    await API.post(insightsCloudUrl('tasks'));
+    dispatch({
+      type: INSIGHTS_CLOUD_SYNC_TASK,
+      payload: {},
+    });
+  } catch (error) {
+    dispatch({
+      type: INSIGHTS_CLOUD_SYNC_TASK,
+      payload: {
+        error: error.message,
+      },
+    });
+  }
 };
