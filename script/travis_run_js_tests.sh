8549de916ee376d626945d795a6200fed106feb5
Ron Lavi <1ronlavi@gmail.com>
2020-05-20 12:47:55 +0300

    Fix branch name in travis js tests script (#211)

--- script/travis_run_js_tests.sh
+++ script/travis_run_js_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-if [[ $( git diff --name-only origin/master..HEAD webpack/ .travis.yml .babelrc .eslintrc package.json | wc -l ) -ne 0 ]]; then
+if [[ $( git diff --name-only origin/foreman_1_24..HEAD webpack/ .travis.yml .babelrc .eslintrc package.json | wc -l ) -ne 0 ]]; then
   npm run test;
   npm run coveralls;
   npm run lint;
