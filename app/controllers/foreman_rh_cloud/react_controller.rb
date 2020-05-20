74117467bd872beccbf217a1626e321ff9c6ddd2
Ron Lavi <1ronlavi@gmail.com>
2020-05-20 15:15:57 +0300

    Fix filter intial org value update (#210)

--- app/controllers/foreman_rh_cloud/react_controller.rb
+++ app/controllers/foreman_rh_cloud/react_controller.rb
@@ -3,7 +3,6 @@ module ForemanRhCloud
     layout "foreman_rh_cloud/application"
 
     def inventory_upload
-      @react_props = { currentOrg: Organization.current&.name }.to_json
     end
   end
 end
--- webpack/ForemanInventoryUpload/Components/InventoryFilter/InventoryFilter.js
+++ webpack/ForemanInventoryUpload/Components/InventoryFilter/InventoryFilter.js
@@ -5,44 +5,35 @@ import { translate as __ } from 'foremanReact/common/I18n';
 import ClearButton from './Components/ClearButton';
 import './inventoryFilter.scss';
 
-class InventoryFilter extends React.Component {
-  componentDidMount() {
-    const { handleFilterChange, initialValue } = this.props;
-    handleFilterChange(initialValue);
-  }
-
-  render() {
-    const { handleFilterChange, handleFilterClear, filterTerm } = this.props;
-
-    return (
-      <form id="inventory_filter_form">
-        <FormGroup controlId="inventory_filter_input">
-          <FormControl
-            value={filterTerm}
-            type="text"
-            placeholder={__('Filter..')}
-            bsSize="lg"
-            onChange={e => handleFilterChange(e.target.value)}
-          />
-          <ClearButton onClear={handleFilterClear} />
-        </FormGroup>
-      </form>
-    );
-  }
-}
+const InventoryFilter = ({
+  handleFilterChange,
+  handleFilterClear,
+  filterTerm,
+}) => (
+  <form id="inventory_filter_form">
+    <FormGroup controlId="inventory_filter_input">
+      <FormControl
+        value={filterTerm}
+        type="text"
+        placeholder={__('Filter..')}
+        bsSize="lg"
+        onChange={e => handleFilterChange(e.target.value)}
+      />
+      <ClearButton onClear={handleFilterClear} />
+    </FormGroup>
+  </form>
+);
 
 InventoryFilter.propTypes = {
   handleFilterChange: PropTypes.func,
   handleFilterClear: PropTypes.func,
   filterTerm: PropTypes.string,
-  initialValue: PropTypes.string,
 };
 
 InventoryFilter.defaultProps = {
   handleFilterChange: noop,
   handleFilterClear: noop,
   filterTerm: '',
-  initialValue: '',
 };
 
 export default InventoryFilter;
