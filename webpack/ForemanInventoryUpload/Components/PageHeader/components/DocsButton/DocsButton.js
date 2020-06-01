8b18b3cff6faada6a1678129781421ab7ff181e9
Ron Lavi <1ronlavi@gmail.com>
2020-06-01 12:56:48 -0400

    move to docsUrl helper

--- webpack/ForemanInventoryUpload/Components/PageHeader/components/DocsButton/DocsButton.js
+++ webpack/ForemanInventoryUpload/Components/PageHeader/components/DocsButton/DocsButton.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Button, Icon } from 'patternfly-react';
-import {
-  INVENTORY_DOCS_LINK,
-  DOCS_BUTTON_TEXT,
-} from '../../../../ForemanInventoryConstants';
+import { DOCS_BUTTON_TEXT } from '../../../../ForemanInventoryConstants';
+import { getInventoryDocsUrl } from '../../../../ForemanInventoryHelpers';
 import './docsButton.scss';
 
 const DocsButton = () => (
   <Button
     className="docs_btn"
-    href={INVENTORY_DOCS_LINK}
+    href={getInventoryDocsUrl()}
     target="_blank"
     rel="noopener noreferrer"
   >
