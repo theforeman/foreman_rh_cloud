f1b7b86c8fd7d901f97ac35e9b1eaf53f14d8449
Ron Lavi <1ronlavi@gmail.com>
2020-06-18 13:59:35 +0300

    Add inventory docs button link (#221)
    * Add inventory docs button link
    * move to docsUrl helper
    
    (cherry picked from commit 6688098abeb4775c0fc36b8be018990017d17f2)

--- webpack/ForemanInventoryUpload/ForemanInventoryHelpers.js
+++ webpack/ForemanInventoryUpload/ForemanInventoryHelpers.js
@@ -1,4 +1,12 @@
+import URI from 'urijs';
 import { foremanUrl } from '../ForemanRhCloudHelpers';
 
 export const inventoryUrl = path =>
   foremanUrl(`/foreman_inventory_upload/${path}`);
+
+export const getInventoryDocsUrl = () =>
+  foremanUrl(
+    `/links/manual/+?root_url=${URI.encode(
+      'https://access.redhat.com/products/subscription-central'
+    )}`
+  );
