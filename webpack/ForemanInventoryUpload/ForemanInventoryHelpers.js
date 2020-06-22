6435710643eaa7373414c1fe3536fbf1296a4acc
laviro <1ronlavi@gmail.com>
2020-06-22 10:44:49 +0300

    downgrade links controller

--- webpack/ForemanInventoryUpload/ForemanInventoryHelpers.js
+++ webpack/ForemanInventoryUpload/ForemanInventoryHelpers.js
@@ -1,12 +1,7 @@
-import URI from 'urijs';
 import { foremanUrl } from '../ForemanRhCloudHelpers';
 
 export const inventoryUrl = path =>
   foremanUrl(`/foreman_inventory_upload/${path}`);
 
 export const getInventoryDocsUrl = () =>
-  foremanUrl(
-    `/links/manual/+?root_url=${URI.encode(
-      'https://access.redhat.com/products/subscription-central'
-    )}`
-  );
+  'https://access.redhat.com/products/subscription-central';
