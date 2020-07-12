82d29d99cf2cb256d1d870769aa30eabf9a22921
Ron Lavi <1ronlavi@gmail.com>
2020-07-12 19:45:41 +0300

    set auto_upload initially "off" (#252) (#256)
    (cherry picked from commit da7b7404e7ec8275c8ff4df93e42a7e203e8e474)
    
    Conflicts:
    	app/models/setting/rh_cloud.rb

--- app/models/setting/rh_cloud.rb
+++ app/models/setting/rh_cloud.rb
@@ -5,7 +5,7 @@ class Setting::RhCloud < Setting
 
     transaction do
       [
-        set('allow_auto_inventory_upload', N_('Allow automatic upload of the host inventory to the Red Hat cloud'), true),
+        set('allow_auto_inventory_upload', N_('Allow automatic upload of the host inventory to the Red Hat cloud'), false),
         set('obfuscate_inventory_hostnames', N_('Obfuscate host names sent to Red Hat cloud'), false),
         set('rh_cloud_token', N_('Authentication token to Red Hat cloud services. Used to authenticate requests to cloud APIs'), 'DEFAULT', N_('Red Hat Cloud token'), nil, encrypted: true),
       ].each { |s| create! s.update(:category => 'Setting::RhCloud')}
