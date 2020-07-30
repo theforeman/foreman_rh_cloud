c8d803a3e5d837a456f040f63b958812694e7399
Shimon Shtein <sshtein@redhat.com>
2020-07-30 16:11:00 +0300

    Delete hits on facet deletion

--- app/models/insights_facet.rb
+++ app/models/insights_facet.rb
@@ -1,3 +1,7 @@
 class InsightsFacet < HostFacets::Base
-  has_many :hits, foreign_key: :host_id, primary_key: :host_id, class_name: 'InsightsHit'
+  has_many :hits,
+    foreign_key: :host_id,
+    primary_key: :host_id,
+    class_name: 'InsightsHit',
+    dependent: :destroy
 end
