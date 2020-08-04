3221e048759d38f3889d46062f1eb121f7110ce1
Shimon Shtein <sshtein@redhat.com>
2020-08-04 11:03:19 +0300

    Downgrade tests

--- webpack/InsightsCloudSync/Components/InsightsSettings/__tests__/InsightsSettingsSelectors.test.js
+++ webpack/InsightsCloudSync/Components/InsightsSettings/__tests__/InsightsSettingsSelectors.test.js
@@ -1,4 +1,4 @@
-import { testSelectorsSnapshotWithFixtures } from '@theforeman/test';
+import { testSelectorsSnapshotWithFixtures } from 'react-redux-test-utils';
 import {
   selectSettings,
   selectInsightsSyncEnabled,
