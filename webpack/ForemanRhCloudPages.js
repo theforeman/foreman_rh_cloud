39eeb0cd80481f547225ef42fd34768b9561e63b
Ron Lavi <1ronlavi@gmail.com>
2020-08-20 16:09:48 +0300

    extend host details page with insights (#310)

--- webpack/ForemanRhCloudPages.js
+++ webpack/ForemanRhCloudPages.js
@@ -1,7 +1,9 @@
 import ForemanInventoryUpload from './ForemanInventoryUpload';
 import InsightsCloudSync from './InsightsCloudSync';
+import InsightsHostDetailsTab from './InsightsHostDetailsTab';
 
 export default [
   { name: 'ForemanInventoryUpload', type: ForemanInventoryUpload },
   { name: 'InsightsCloudSync', type: InsightsCloudSync },
+  { name: 'InsightsHostDetailsTab', type: InsightsHostDetailsTab },
 ];
