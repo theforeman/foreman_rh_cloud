af9cb315212b3c8a1fbb58185cede551aac81991
Ron Lavi <1ronlavi@gmail.com>
2020-10-05 14:19:18 +0300

    Add UI switcher for exclude packages setting (#352)
    * Add UI switcher for exclude packages setting
    
    * Add integration test
    (cherry picked from commit 43d416d3dd5239fc56cee451f7bd447329438507)

---
+++ webpack/ForemanInventoryUpload/Components/ExcludePackagesSwitcher/__tests__/ExcludePackagesSwitcherActions.test.js
@@ -0,0 +1,21 @@
+import { testActionSnapshotWithFixtures } from '@theforeman/test';
+import API from 'foremanReact/API';
+import { handleToggle } from '../ExcludePackagesSwitcherActions';
+import { handleToggleResponse } from '../ExcludePackagesSwitcher.fixtures';
+
+jest.mock('foremanReact/API');
+API.post.mockImplementation(async () => handleToggleResponse);
+
+const fixtures = {
+  'should handleToggle': () => handleToggle(),
+  'should invoke toast notification upon failure of handleToggle': () => {
+    API.post.mockImplementationOnce(() =>
+      Promise.reject(new Error('test error'))
+    );
+
+    return handleToggle();
+  },
+};
+
+describe('ExcludePackagesSwitcher actions', () =>
+  testActionSnapshotWithFixtures(fixtures));
--- webpack/ForemanInventoryUpload/Components/InventorySettings/InventorySettings.js
+++ webpack/ForemanInventoryUpload/Components/InventorySettings/InventorySettings.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { translate as __ } from 'foremanReact/common/I18n';
 import AutoUploadSwitcher from '../AutoUploadSwitcher';
 import HostObfuscationSwitcher from '../HostObfuscationSwitcher';
+import ExcludePackagesSwitcher from '../ExcludePackagesSwitcher';
 import './inventorySettings.scss';
 
 const InventorySettings = () => (
   <div className="inventory_settings">
     <h3>{__('Settings')}</h3>
     <AutoUploadSwitcher />
+    <ExcludePackagesSwitcher />
     <HostObfuscationSwitcher />
   </div>
 );
