a92be07d8806db58f0c40b154f46ccaab2f09f15
Ron Lavi <1ronlavi@gmail.com>
2020-10-14 07:45:31 -0400

    Refactor auto-upload-switcher error handling

--- webpack/ForemanInventoryUpload/Components/AutoUploadSwitcher/__tests__/AutoUploadSwitcherActions.test.js
+++ webpack/ForemanInventoryUpload/Components/AutoUploadSwitcher/__tests__/AutoUploadSwitcherActions.test.js
@@ -1,13 +1,23 @@
 import { testActionSnapshotWithFixtures } from '@theforeman/test';
 import API from 'foremanReact/API';
 import { handleToggle } from '../AutoUploadSwitcherActions';
-import { handleToggleResponse } from '../AutoUploadSwitcher.fixtures';
+import {
+  handleToggleResponse,
+  currentAutoUploadEnabled,
+} from '../AutoUploadSwitcher.fixtures';
 
 jest.mock('foremanReact/API');
 API.post.mockImplementation(async () => handleToggleResponse);
 
 const fixtures = {
-  'should handleToggle': () => handleToggle(),
+  'should handleToggle': () => handleToggle(currentAutoUploadEnabled),
+  'should handleToggle with error': () => {
+    API.post.mockImplementationOnce(() =>
+      Promise.reject(new Error('Network error!'))
+    );
+
+    return handleToggle(currentAutoUploadEnabled);
+  },
 };
 
 describe('AutoUploadSwitcher actions', () =>
