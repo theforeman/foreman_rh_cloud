1fd3f129cbbf3823271e5a23d2737143b817b8b5
Ron Lavi <1ronlavi@gmail.com>
2020-10-26 08:39:27 -0400

    Fix hits item structure to match changed response

--- webpack/InsightsHostDetailsTab/__tests__/InsightsTab.fixtures.js
+++ webpack/InsightsHostDetailsTab/__tests__/InsightsTab.fixtures.js
@@ -2,20 +2,18 @@ export const hostID = 1234;
 
 export const hits = [
   {
-    insights_hit: {
-      hostname: 'my-host.example.com',
-      rhel_version: '7.8',
-      uuid: '4739b323-a343-4e89-b71b-81991b8dc656',
-      last_seen: '2020-08-19T04:43:09.068706Z',
-      title:
-        'New Ansible Engine packages are inaccessible when dedicated Ansible repo is not enabled',
-      solution_url: 'https://access.redhat.com/node/3359651',
-      total_risk: 2,
-      likelihood: 2,
-      publish_date: '2018-04-16T10:03:16Z',
-      results_url:
-        'https://cloud.redhat.com/insights/advisor/recommendations/ansible_deprecated_repo%7CANSIBLE_DEPRECATED_REPO/4739b323-a343-4e89-b71b-81991b8dc656/',
-    },
+    hostname: 'my-host.example.com',
+    rhel_version: '7.8',
+    uuid: '4739b323-a343-4e89-b71b-81991b8dc656',
+    last_seen: '2020-08-19T04:43:09.068706Z',
+    title:
+      'New Ansible Engine packages are inaccessible when dedicated Ansible repo is not enabled',
+    solution_url: 'https://access.redhat.com/node/3359651',
+    total_risk: 2,
+    likelihood: 2,
+    publish_date: '2018-04-16T10:03:16Z',
+    results_url:
+      'https://cloud.redhat.com/insights/advisor/recommendations/ansible_deprecated_repo%7CANSIBLE_DEPRECATED_REPO/4739b323-a343-4e89-b71b-81991b8dc656/',
   },
 ];
 
