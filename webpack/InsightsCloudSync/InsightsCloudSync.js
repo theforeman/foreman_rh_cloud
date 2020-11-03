48aabcfee5b0acef773ec764d3778372f1d2df26
Shimon Shtein <sshtein@redhat.com>
2020-11-03 16:07:18 +0200

    Rename RHSM token to Red Hat token

--- webpack/InsightsCloudSync/InsightsCloudSync.js
+++ webpack/InsightsCloudSync/InsightsCloudSync.js
@@ -19,7 +19,7 @@ const InsightsCloudSync = ({ data: { settingsUrl }, syncInsights }) => {
              recommendations output for hosts managed here`)}
           </p>
           <p>
-            {__(`1. Obtain an RHSM API token: `)}
+            {__(`1. Obtain an Red Hat API token: `)}
             <a
               href="https://access.redhat.com/management/api"
               target="_blank"
