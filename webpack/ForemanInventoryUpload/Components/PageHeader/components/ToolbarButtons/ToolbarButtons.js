35f580794eec058da2dcf6583e6fe66a847f9242
Maria <mariaagaphonchev@gmail.com>
2021-01-18 18:10:04 +0200

    Move inventory toolbar buttons to title (#440)

--- webpack/ForemanInventoryUpload/Components/PageHeader/components/ToolbarButtons/ToolbarButtons.js
+++ webpack/ForemanInventoryUpload/Components/PageHeader/components/ToolbarButtons/ToolbarButtons.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import DocsButton from '../DocsButton';
-import HistoryButton from '../HistoryButton';
 import SyncButton from '../SyncButton';
 import './toolbarButtons.scss';
 
 const ToolbarButtons = () => (
   <div className="inventory_toolbar_buttons">
     <SyncButton />
-    <HistoryButton />
-    <DocsButton />
   </div>
 );
 
--- webpack/ForemanInventoryUpload/ForemanInventoryUpload.js
+++ webpack/ForemanInventoryUpload/ForemanInventoryUpload.js
@@ -2,15 +2,10 @@ import React from 'react';
 import PageLayout from 'foremanReact/routes/common/PageLayout/PageLayout';
 import AccountList from './Components/AccountList';
 import PageHeader from './Components/PageHeader';
-import { INVENTORY_PAGE_TITLE } from './ForemanInventoryConstants';
 
 const ForemanInventoryUpload = () => (
   <div className="rh-cloud-page">
-    <PageLayout
-      header={INVENTORY_PAGE_TITLE}
-      searchable={false}
-      beforeToolbarComponent={<PageHeader />}
-    >
+    <PageLayout searchable={false} beforeToolbarComponent={<PageHeader />}>
       <AccountList />
     </PageLayout>
   </div>
