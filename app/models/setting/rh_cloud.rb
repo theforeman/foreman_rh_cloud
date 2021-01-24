c63f276c5b588aa05a4915c0e2bfeca8a7a174fa
Shimon Shtein <sshtein@redhat.com>
2021-01-24 13:39:26 +0200

    Add settings names (#450)

--- app/models/setting/rh_cloud.rb
+++ app/models/setting/rh_cloud.rb
@@ -3,12 +3,12 @@ class Setting::RhCloud < Setting
     return unless ActiveRecord::Base.connection.table_exists?('settings')
     return unless super
     [
-      set('allow_auto_inventory_upload', N_('Allow automatic upload of the host inventory to the Red Hat cloud'), false),
-      set('allow_auto_insights_sync', N_('Allow recommendations synchronization from Red Hat cloud'), false),
-      set('obfuscate_inventory_hostnames', N_('Obfuscate host names sent to Red Hat cloud'), false),
-      set('obfuscate_inventory_ips', N_('Obfuscate ip addresses sent to Red Hat cloud'), false),
+      set('allow_auto_inventory_upload', N_('Allow automatic upload of the host inventory to the Red Hat cloud'), false, N_('Allow automatic inventory uploads')),
+      set('allow_auto_insights_sync', N_('Allow recommendations synchronization from Red Hat cloud'), false, N_('Allow recommendations synchronization')),
+      set('obfuscate_inventory_hostnames', N_('Obfuscate host names sent to Red Hat cloud'), false, N_('Obfuscate host names')),
+      set('obfuscate_inventory_ips', N_('Obfuscate ip addresses sent to Red Hat cloud'), false, N_('Obfuscate IPs')),
       set('rh_cloud_token', N_('Authentication token to Red Hat cloud services. Used to authenticate requests to cloud APIs'), 'DEFAULT', N_('Red Hat Cloud token'), nil, encrypted: true),
-      set('exclude_installed_packages', N_('Exclude installed packages from Red Hat cloud inventory report'), false),
+      set('exclude_installed_packages', N_('Exclude installed packages from Red Hat cloud inventory report'), false, N_("Don't upload installed packages")),
     ]
   end
 
