f96fdb3b871e4a709b5869a97d44d377dcae826b
Ron Lavi <1ronlavi@gmail.com>
2021-02-16 20:28:42 +0200

    use empty? instead of length > 1

--- app/controllers/foreman_inventory_upload/accounts_controller.rb
+++ app/controllers/foreman_inventory_upload/accounts_controller.rb
@@ -24,7 +24,7 @@ module ForemanInventoryUpload
         autoUploadEnabled: Setting[:allow_auto_inventory_upload],
         hostObfuscationEnabled: Setting[:obfuscate_inventory_hostnames],
         ipsObfuscationEnabled: Setting[:obfuscate_inventory_ips],
-        cloudToken: Setting[:rh_cloud_token].length > 1,
+        cloudToken: !Setting[:rh_cloud_token].empty?,
         excludePackages: Setting[:exclude_installed_packages],
         accounts: accounts,
         CloudConnectorStatus: cloud_connector_status,
