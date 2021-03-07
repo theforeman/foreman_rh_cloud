e43cebd6560997af79a0a9213b047f1427301c81
Ron Lavi <1ronlavi@gmail.com>
2021-03-07 11:18:05 +0200

    handle accounts by label nor id (#495) (#503)
    (cherry picked from commit a8965e323bd3772923ada0f6c8bdca4da05978dc)

--- webpack/ForemanInventoryUpload/Components/AccountList/Components/ListItem/ListItem.fixtures.js
+++ webpack/ForemanInventoryUpload/Components/AccountList/Components/ListItem/ListItem.fixtures.js
@@ -1,1 +1,10 @@
-export const props = { accountID: '1' };
+export const props = {
+  label: 'test',
+  account: {
+    test: {
+      generate_report_status: 'unknown',
+      upload_report_status: 'unknown',
+      id: 1,
+    },
+  },
+};
