46d27d152cd15e9eb21b205acf0abfcdd64ef3e4
Ron Lavi <1ronlavi@gmail.com>
2021-04-25 10:02:53 +0000

    order remediations in the modal by resolutions length

--- webpack/InsightsCloudSync/Components/RemediationModal/RemediationHelpers.js
+++ webpack/InsightsCloudSync/Components/RemediationModal/RemediationHelpers.js
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import React from 'react';
+import { orderBy } from 'lodash';
 import Resolutions from './Resolutions';
 
 export const modifyRows = (remediations, setResolutions, setHostsIds) => {
@@ -7,32 +8,34 @@ export const modifyRows = (remediations, setResolutions, setHostsIds) => {
 
   const resolutionToSubmit = [];
   const hostsIdsToSubmit = new Set();
-  const modifiedRemediations = remediations
-    .asMutable()
-    .map(({ id, host_id, hostname, title, resolutions, reboot }) => {
-      hostsIdsToSubmit.add(host_id);
-      const selectedResolution = resolutions[0]?.id;
-      resolutionToSubmit.push({
-        hit_id: id,
-        resolution_id: selectedResolution /** defaults to the first resolution if many */,
-      });
-      return {
-        cells: [
-          hostname,
-          title,
-          <div>
-            <Resolutions
-              hit_id={id}
-              resolutions={resolutions}
-              setResolutions={setResolutions}
-              selectedResolution={selectedResolution}
-            />
-          </div>,
-          reboot,
-        ],
-        id,
-      };
+  const modifiedRemediations = orderBy(
+    remediations.asMutable(),
+    [r => r.resolutions?.length || 0],
+    ['desc']
+  ).map(({ id, host_id, hostname, title, resolutions, reboot }) => {
+    hostsIdsToSubmit.add(host_id);
+    const selectedResolution = resolutions[0]?.id;
+    resolutionToSubmit.push({
+      hit_id: id,
+      resolution_id: selectedResolution /** defaults to the first resolution if many */,
     });
+    return {
+      cells: [
+        hostname,
+        title,
+        <div>
+          <Resolutions
+            hit_id={id}
+            resolutions={resolutions}
+            setResolutions={setResolutions}
+            selectedResolution={selectedResolution}
+          />
+        </div>,
+        reboot,
+      ],
+      id,
+    };
+  });
 
   setResolutions(resolutionToSubmit);
   setHostsIds(Array.from(hostsIdsToSubmit));
