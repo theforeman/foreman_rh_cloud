c97ddec5d270f10d7f21d6a98c80c7aa366b3de7
Shimon Shtein <sshtein@redhat.com>
2021-06-15 12:16:31 +0300

    Omit malformed bios_uuid values (#551)
    * Omit malformed bios_uuid values
    
    * Add exceptions to UUID fields

--- test/factories/inventory_upload_factories.rb
+++ test/factories/inventory_upload_factories.rb
@@ -49,7 +49,7 @@ end
 
 FactoryBot.define do
   factory :katello_subscription_facets, :aliases => [:subscription_facet], :class => ::Katello::Host::SubscriptionFacet do
-    sequence(:uuid) { |n| "uuid-#{n}-#{rand(500)}" }
+    sequence(:uuid) { |n| "00000000-%<n>04d-%<r>04d-0000-000000000000" % {n: n, r: rand(500)} }
     facts { { 'memory.memtotal' => "12 GB" } }
   end
 end
