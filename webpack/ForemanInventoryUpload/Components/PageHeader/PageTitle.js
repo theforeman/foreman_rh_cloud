c81764507835a42e655da9966332d38930352699
Ron Lavi <1ronlavi@gmail.com>
2021-08-04 18:32:37 +0300

    Add frontend for network status (#622)

--- webpack/ForemanInventoryUpload/Components/PageHeader/PageTitle.js
+++ webpack/ForemanInventoryUpload/Components/PageHeader/PageTitle.js
@@ -9,14 +9,18 @@ import {
   INVENTORY_PAGE_TITLE,
   ACTIONS_HISTORY_BUTTON_TEXT,
   DOCS_BUTTON_TEXT,
+  CLOUD_PING_TITLE,
 } from '../../ForemanInventoryConstants';
 import {
   getActionsHistoryUrl,
   getInventoryDocsUrl,
 } from '../../ForemanInventoryHelpers';
+import CloudPingModal from './components/CloudPingModal';
 
 const PageTitle = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [showPingModal, setPingModal] = useState(false);
+  const togglePingModal = () => setPingModal(v => !v);
   const dropdownItems = [
     <DropdownItem
       key="tasks-history-button"
@@ -34,6 +38,9 @@ const PageTitle = () => {
     >
       {DOCS_BUTTON_TEXT}
     </DropdownItem>,
+    <DropdownItem key="cloud-ping" onClick={togglePingModal}>
+      {CLOUD_PING_TITLE}
+    </DropdownItem>,
   ];
   return (
     <div className="row form-group inventory-upload-header-title">
@@ -46,6 +53,11 @@ const PageTitle = () => {
         isPlain
         dropdownItems={dropdownItems}
         position={DropdownPosition.right}
+      />
+      <CloudPingModal
+        isOpen={showPingModal}
+        toggle={togglePingModal}
+        title={CLOUD_PING_TITLE}
       />
     </div>
   );
