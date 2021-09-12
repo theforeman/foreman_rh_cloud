888ab08cf450d87593a5e82234a47bf58b844af9
Shimon Shtein <sshtein@redhat.com>
2021-09-12 18:02:41 +0300

    Look for short hostname for foreman host

---
+++ test/unit/foreman_rh_cloud_self_host_test.rb
@@ -0,0 +1,28 @@
+require 'test_plugin_helper'
+
+class ForemanRhCloudSelfHostTest < ActiveSupport::TestCase
+  setup do
+    # reset cached value
+    ForemanRhCloud.instance_variable_set(:@foreman_host, nil)
+  end
+
+  test 'finds host by fullname' do
+    @domain
+    @host = FactoryBot.create(:host, :managed)
+    ForemanRhCloud.expects(:foreman_host_name).returns(@host.name)
+
+    actual = ForemanRhCloud.foreman_host
+
+    assert_not_nil actual
+  end
+
+  test 'finds host by shortname' do
+    @host = FactoryBot.create(:host, :managed)
+    Host.where(name: @host.name).update_all(name: @host.shortname)
+    ForemanRhCloud.expects(:foreman_host_name).returns(@host.name)
+
+    actual = ForemanRhCloud.foreman_host
+
+    assert_not_nil actual
+  end
+end
