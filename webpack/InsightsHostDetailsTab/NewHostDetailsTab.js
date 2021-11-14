3c7fdbdbf9209281710ad9dc83be462441a9bbd3
Ron Lavi <1ronlavi@gmail.com>
2021-11-14 11:36:21 +0200

    Add link from host's insights tab to insights page
    link from the tab to '/foreman_rh_cloud/insights_cloud'

--- webpack/InsightsHostDetailsTab/NewHostDetailsTab.js
+++ webpack/InsightsHostDetailsTab/NewHostDetailsTab.js
@@ -1,8 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchBar from 'foremanReact/components/SearchBar';
-import { Grid, GridItem } from '@patternfly/react-core';
+import { translate as __ } from 'foremanReact/common/I18n';
+import {
+  Grid,
+  GridItem,
+  Dropdown,
+  DropdownItem,
+  KebabToggle,
+} from '@patternfly/react-core';
 import InsightsTable from '../InsightsCloudSync/Components/InsightsTable';
 import RemediationModal from '../InsightsCloudSync/Components/RemediationModal';
 import Pagination from '../InsightsCloudSync/Components/InsightsTable/Pagination';
@@ -19,6 +26,18 @@ const NewHostDetailsTab = ({ hostName, router }) => {
 
   const onSearch = q => dispatch(fetchInsights({ query: q, page: 1 }));
 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownItems = [
+    <DropdownItem
+      key="insights-link"
+      onClick={() =>
+        router.push({ pathname: '/foreman_rh_cloud/insights_cloud' })
+      }
+    >
+      {__('Go to Insights page')}
+    </DropdownItem>,
+  ];
+
   return (
     <Grid id="new_host_details_insights_tab" hasGutter>
       <GridItem span={5}>
@@ -30,6 +49,16 @@ const NewHostDetailsTab = ({ hostName, router }) => {
       </GridItem>
       <GridItem span={4}>
         <RemediationModal />
+        <Dropdown
+          className="insights-dropdown"
+          onSelect={() => setIsDropdownOpen(false)}
+          toggle={
+            <KebabToggle onToggle={isOpen => setIsDropdownOpen(isOpen)} />
+          }
+          isOpen={isDropdownOpen}
+          isPlain
+          dropdownItems={dropdownItems}
+        />
       </GridItem>
       <GridItem span={3}>
         <Pagination variant="top" isCompact />
