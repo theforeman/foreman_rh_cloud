29a114fd7fa1399ea0f29676d2b0cd9448169de9
Shimon Shtein <sshtein@redhat.com>
2022-01-13 16:20:35 +0200

    Build container action (#677) (#683)
    * Add build and publish action
    
    * Switch to pnpm

---
+++ .github/images/npm_install_plugins.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const childProcess = require('child_process');
+const { packageJsonDirs } = require('./plugin_webpack_directories');
+
+packageJsonDirs('pipe').forEach(pluginPath => {
+  childProcess.spawn('pnpm', ['install', '--no-optional'], {
+    env: process.env,
+    cwd: pluginPath,
+    stdio: 'inherit',
+  });
+});
