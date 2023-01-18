d0177c8ee617e463bdca0f45c6406c39aebc5d3c
Chris Roberts <chrobert@redhat.com>
2023-01-18 16:09:44 +0000

    update metadata

--- lib/foreman_inventory_upload/generators/metadata.rb
+++ lib/foreman_inventory_upload/generators/metadata.rb
@@ -27,7 +27,9 @@ module ForemanInventoryUpload
         @stream.object do
           @stream.simple_field('report_id', Foreman.uuid)
           @stream.simple_field('host_inventory_api_version', '1.0')
-          @stream.simple_field('source', 'Satellite')
+          Setting[:obfuscate_inventory_hostnames] ? @stream.simple_field('source', 'Satellite') : @stream.simple_field('source', ForemanRhCloud.foreman_host_name)
+          @stream.simple_field('IP address', ForemanRhCloud.foreman_host.facts['network::ipv4_address']) unless Setting[:obfuscate_inventory_ips]
+          @stream.simple_field('bios UUID', ForemanRhCloud.foreman_host.facts['dmi::system::uuid'])
           @stream.simple_field('source_metadata', metadata)
           @stream.object_field('report_slices', :last) do
             yield(self)
