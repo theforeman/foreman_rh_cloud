theforeman/foreman_rh_cloud

commit 5b468889459e0318699b73370049a914a2ccef32

rename plugin to foreman_rh_cloud (#125)

locale/gemspec.rb
@@ -1,2 +1,2 @@
-# Matches foreman_yupana.gemspec
-_('TODO: Description of ForemanYupana.')
+# Matches foreman_rh_cloud.gemspec
+_('TODO: Description of ForemanRhCloud.')

.travis.yml
@@ -11,7 +11,7 @@ install:
   - unset BUNDLE_GEMFILE
   - cd ..
   - git clone https://github.com/theforeman/foreman.git -b ${FOREMAN_CORE_BRANCH} --depth 1
-  - cd ${FOREMAN_PLUGIN_NAME}
+  - cd ${REPO_NAME}
   - gem install bundler
   - nvm install 10
   - node --version
@@ -20,7 +20,7 @@ install:
   - bundle install --jobs=3 --retry=3
   - npm install eslint
   - cd ../foreman
-  - echo "gemspec :path => '../${FOREMAN_PLUGIN_NAME}'" > bundler.d/${FOREMAN_PLUGIN_NAME}.local.rb
+  - echo "gemspec :path => '../${REPO_NAME}'" > bundler.d/${FOREMAN_PLUGIN_NAME}.local.rb
   - echo "gem 'katello'" > bundler.d/katello.local.rb
   - ln -s settings.yaml.test config/settings.yaml
   - ln -s database.yml.example config/database.yml
@@ -33,14 +33,15 @@ script:
   - bundle exec rake test:${FOREMAN_PLUGIN_NAME}
   - bundle exec rake "plugin:assets:precompile[${FOREMAN_PLUGIN_NAME}]" RAILS_ENV=production
   - bundle exec rake "${FOREMAN_PLUGIN_NAME}:rubocop"
-  - cd ../${FOREMAN_PLUGIN_NAME}
+  - cd ../${REPO_NAME}
   - npm run test;
   - # npm run coveralls;
   - npm run lint;
 env:
   global:
     - TESTOPTS=-v
-    - FOREMAN_PLUGIN_NAME=foreman_inventory_upload
+    - FOREMAN_PLUGIN_NAME=foreman_rh_cloud
+    - REPO_NAME=foreman_inventory_upload #revert after github repo rename.
   matrix:
     - FOREMAN_CORE_BRANCH=develop
 addons:

.tx/config
@@ -1,8 +1,8 @@
 [main]
 host = https://www.transifex.com
 
-[foreman.foreman_yupana]
-file_filter = locale/<lang>/foreman_yupana.edit.po
-source_file = locale/foreman_yupana.pot
+[foreman.foreman_rh_cloud]
+file_filter = locale/<lang>/foreman_rh_cloud.edit.po
+source_file = locale/foreman_rh_cloud.pot
 source_lang = en
 type = PO

Rakefile
@@ -14,7 +14,7 @@ end
 
 RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_dir = 'rdoc'
-  rdoc.title    = 'ForemanInventoryUpload'
+  rdoc.title    = 'ForemanRhCloud'
   rdoc.options << '--line-numbers'
   rdoc.rdoc_files.include('README.rdoc')
   rdoc.rdoc_files.include('lib/**/*.rb')

app/views/dashboard/_inventory_upload_widget.html.erb
@@ -1,2 +1,0 @@
-<h4 class="header ca"><%= _('ForemanInventoryUpload') %></h4>
-<%= _('Widget content') %>

app/views/foreman_inventory_upload/layouts/react.html.erb
@@ -1,9 +1,9 @@
 <% content_for(:javascripts) do %>
-  <%= webpacked_plugins_js_for :'foreman_inventory_upload' %>
+  <%= webpacked_plugins_js_for :'foreman_rh_cloud' %>
 <% end %>
 
 <% content_for(:stylesheets) do %>
-    <%= webpacked_plugins_css_for :'foreman_inventory_upload' %>
+    <%= webpacked_plugins_css_for :'foreman_rh_cloud' %>
 <% end %>
 
 <% content_for(:content) do %>

foreman_rh_cloud.gemspec
@@ -1,15 +1,15 @@
-require File.expand_path('lib/foreman_inventory_upload/version', __dir__)
+require File.expand_path('lib/foreman_rh_cloud/version', __dir__)
 
 Gem::Specification.new do |s|
-  s.name        = 'foreman_inventory_upload'
-  s.version     = ForemanInventoryUpload::VERSION
+  s.name        = 'foreman_rh_cloud'
+  s.version     = ForemanRhCloud::VERSION
   s.license     = 'GPL-3.0'
-  s.authors     = ['Inventory upload team']
+  s.authors     = ['Foreman Red Hat Cloud team']
   s.email       = ['rlavi@redhat.com, sshtein@redhat.com']
-  s.homepage    = 'https://github.com/theforeman/foreman_inventory_upload'
-  s.summary     = 'Summary of ForemanInventoryUpload.'
+  s.homepage    = 'https://github.com/theforeman/foreman_rh_cloud'
+  s.summary     = 'Summary of ForemanRhCloud.'
   # also update locale/gemspec.rb
-  s.description = 'Foreman plugin that process & upload data to cloud based host inventory'
+  s.description = 'Foreman plugin that process & upload data to Red Hat Cloud'
 
   s.files = Dir['{app,config,db,lib,locale,webpack}/**/*'] +
             ['LICENSE', 'Rakefile', 'README.md'] +

lib/foreman_inventory_upload.rb
@@ -1,5 +1,3 @@
-require 'foreman_inventory_upload/engine'
-
 module ForemanInventoryUpload
   def self.base_folder
     # in production setup, where selinux is enabled, we only have rights to

commit 6997a1a2c2182d5283ba609f2a72b4f1efe6a096

Match locale/gemspec to the real gemspec description
Fixes: 5b468889459e0318699b73370049a914a2ccef32

locale/gemspec.rb
@@ -1,2 +1,2 @@
 # Matches foreman_rh_cloud.gemspec
-_('TODO: Description of ForemanRhCloud.')
+_('Foreman plugin that process & upload data to Red Hat Cloud')