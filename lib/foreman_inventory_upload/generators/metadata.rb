f3044b4919d368a3a169b27e5c5788344c468e62
Chris Roberts <chrobert@redhat.com>
2023-03-12 05:26:24 -0400

    Update report metadata to include system info (#798)

--- lib/foreman_inventory_upload/generators/metadata.rb
+++ lib/foreman_inventory_upload/generators/metadata.rb
@@ -1,6 +1,7 @@
 module ForemanInventoryUpload
   module Generators
     class Metadata
+      include FactHelpers
       def initialize(output = [])
         @stream = JsonStream.new(output)
       end
@@ -28,6 +29,9 @@ module ForemanInventoryUpload
           @stream.simple_field('report_id', Foreman.uuid)
           @stream.simple_field('host_inventory_api_version', '1.0')
           @stream.simple_field('source', 'Satellite')
+          @stream.simple_field('reporting_host_name', hostname_match)
+          @stream.simple_field('reporting_host_ips', host_ips(ForemanRhCloud.foreman_host))
+          @stream.simple_field('reporting_host_bios_uuid', bios_uuid(ForemanRhCloud.foreman_host))
           @stream.simple_field('source_metadata', metadata)
           @stream.object_field('report_slices', :last) do
             yield(self)
--- test/unit/metadata_generator_test.rb
+++ test/unit/metadata_generator_test.rb
@@ -2,15 +2,36 @@ require 'test_plugin_helper'
 
 class MetadataGeneratorTest < ActiveSupport::TestCase
   setup do
+    ForemanRhCloud.instance_variable_set(:@foreman_host, nil)
   end
 
   test 'generates an empty report' do
+    @host = FactoryBot.create(:host, :managed)
+    ForemanRhCloud.expects(:foreman_host_name).returns(@host.name)
+    hostname = `uname -n`.chomp
     generator = ForemanInventoryUpload::Generators::Metadata.new
 
     json_str = generator.render do
     end
     actual = JSON.parse(json_str.join("\n"))
+    assert_not_nil actual['report_id']
+    assert_equal 'Satellite', actual['source']
+    assert_equal hostname, actual['reporting_host_name']
+    assert_not_nil (actual_metadata = actual['source_metadata'])
+    assert_equal ForemanRhCloud::VERSION, actual_metadata['foreman_rh_cloud_version']
+    assert_equal({}, actual['report_slices'])
+  end
 
+  test 'generates an empty report with hidden hostname and ip' do
+    Setting[:obfuscate_inventory_hostnames] = true
+    Setting[:obfuscate_inventory_ips] = true
+    @host = FactoryBot.create(:host, :managed)
+    ForemanRhCloud.expects(:foreman_host_name).returns(@host.name)
+    generator = ForemanInventoryUpload::Generators::Metadata.new
+
+    json_str = generator.render do
+    end
+    actual = JSON.parse(json_str.join("\n"))
     assert_not_nil actual['report_id']
     assert_equal 'Satellite', actual['source']
     assert_not_nil (actual_metadata = actual['source_metadata'])
@@ -19,6 +40,9 @@ class MetadataGeneratorTest < ActiveSupport::TestCase
   end
 
   test 'generates a report for a single slice' do
+    @host = FactoryBot.create(:host, :managed)
+    ForemanRhCloud.expects(:foreman_host_name).returns(@host.name)
+    hostname = `uname -n`.chomp
     generator = ForemanInventoryUpload::Generators::Metadata.new
 
     json_str = generator.render do |gen|
@@ -31,6 +55,7 @@ class MetadataGeneratorTest < ActiveSupport::TestCase
 
     assert_not_nil actual['report_id']
     assert_equal 'Satellite', actual['source']
+    assert_equal hostname, actual['reporting_host_name']
     assert_not_nil(slices = actual['report_slices'])
     assert_not_nil(slice = slices['test_123'])
     assert_equal 1, slice['number_hosts']
