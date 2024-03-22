210d322602eb3406f85c91cab2605aee9d456c36
Bernhard Suttner <suttner@atix.de>
2024-03-22 13:55:22 +0100

    Fix scheduled task for uploading report failed (#874)

--- app/controllers/api/v2/rh_cloud/inventory_controller.rb
+++ app/controllers/api/v2/rh_cloud/inventory_controller.rb
@@ -18,10 +18,10 @@ module Api
 
         api :POST, "/organizations/:organization_id/rh_cloud/report", N_("Start report generation")
         param :organization_id, Integer, required: true, desc: N_("Set the current organization context for the request")
-        param :disconnected, :bool, required: false, desc: N_('Generate the report, but do not upload')
+        param :disconnected, :bool, required: false, default: false, desc: N_('Generate the report, but do not upload')
         def generate_report
           organization_id = params[:organization_id]
-          disconnected = params[:disconnected]
+          disconnected = params[:disconnected] || false
 
           start_report_generation(organization_id, disconnected)
 
