aac1a2e62d7997af22d2af33e2371db1f0da4306
Shimon Shtein <sshtein@redhat.com>
2025-06-25 05:23:53 -0400

    Observe package upload actions in Katello

---
+++ app/controllers/concerns/insights_cloud/package_profile_upload_extensions.rb
@@ -0,0 +1,28 @@
+module InsightsCloud
+  module PackageProfileUploadExtensions
+    extend ActiveSupport::Concern
+
+    included do
+      after_action :generate_host_report, only: [:upload_package_profile, :upload_profiles]
+    end
+
+    def generate_host_report
+      return unless ForemanRhCloud.with_local_advisor_engine?
+
+      ForemanTasks.async_task(
+        ForemanInventoryUpload::Async::GenerateReportJob,
+        ForemanInventoryUpload.generated_reports_folder,
+        @host.organization_id,
+        false,
+        { id: @host.id }
+      )
+
+      # in IoP case, the hosts are identified by the sub-man ID, and we can assume they already
+      # exist in the local inventory. This will also handle facet creation for new hosts.
+      unless @host.insights
+        insights_facet = @host.build_insights(uuid: @host.subscription_facet.uuid)
+        insights_facet.save
+      end
+    end
+  end
+end
