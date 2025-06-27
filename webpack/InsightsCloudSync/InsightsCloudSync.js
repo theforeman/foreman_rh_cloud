57f8b59365eb1323685edb6edb74eeb9f84eda02
Jeremy Lenz <jlenz@redhat.com>
2025-06-27 15:47:17 -0400

    add dynamic manifest location

--- webpack/InsightsCloudSync/InsightsCloudSync.js
+++ webpack/InsightsCloudSync/InsightsCloudSync.js
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import PageLayout from 'foremanReact/routes/common/PageLayout/PageLayout';
+import { TextInput, Button } from '@patternfly/react-core';
 import { ScalprumComponent } from '@scalprum/react-core';
 import {
   ScalprumContextWrapper,
@@ -62,24 +63,41 @@ InsightsCloudSync.defaultProps = {
 
 const LocalAdvisorPlaceholder = props => {
   const { config, setConfig } = useContext(ScalprumContext);
-  const path = 'apps/advisor';
-  const scope = 'advisor';
+  const [scope, setScope] = useState('advisor');
+  const path = `apps/${scope}`;
   const module = './SatelliteDemoComponent';
+  const manifestLocation = `https://stage.foo.redhat.com:1337/${path}/fed-mods.json`;
+
+  const [value, setValue] = useState(scope);
 
   useEffect(() => {
     setConfig({
       [scope]: {
         name: scope,
-        manifestLocation: `https://stage.foo.redhat.com:1337/${path}/fed-mods.json`,
+        manifestLocation,
         cdnPath: `${window.location.origin}/scalprum/${path}/`,
       },
     });
-  }, [setConfig]);
+  }, [setConfig, scope, path, manifestLocation]);
 
   return (
-    config[scope] && (
-      <ScalprumComponent scope={scope} module={module} {...props} />
-    )
+    <>
+      <span>
+        <TextInput
+          value={value}
+          type="text"
+          onChange={(_event, val) => setValue(val)}
+          aria-label="manifest location"
+        />
+        <Button variant="primary" onClick={() => setScope(value)}>
+          Set scope
+        </Button>
+      </span>
+      <pre style={{ paddingBottom: '2em' }}>{manifestLocation}</pre>
+      {config[scope] && (
+        <ScalprumComponent scope={scope} module={module} {...props} />
+      )}
+    </>
   );
 };
 
