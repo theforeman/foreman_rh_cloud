f0b79d92747a16429b017aa1cf559320e50112ba
Partha Aji <paji@redhat.com>
2025-07-07 09:24:03 -0400

    rhel advisor sync and package changes

--- webpack/InsightsCloudSync/InsightsCloudSync.js
+++ webpack/InsightsCloudSync/InsightsCloudSync.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import PageLayout from 'foremanReact/routes/common/PageLayout/PageLayout';
+import { TextInput, Button } from '@patternfly/react-core';
+import { ScalprumComponent } from '@scalprum/react-core';
+import {
+  ScalprumContextWrapper,
+  ScalprumContext,
+} from '../common/ScalprumModule/ScalprumContext';
 import InsightsTable from './Components/InsightsTable';
+import { useAdvisorEngineConfig } from '../common/Hooks/ConfigHooks';
 import RemediationModal from './Components/RemediationModal';
 import {
   INSIGHTS_SYNC_PAGE_TITLE,
@@ -54,4 +61,64 @@ InsightsCloudSync.defaultProps = {
   query: '',
 };
 
-export default InsightsCloudSync;
+const LocalAdvisorPlaceholder = props => {
+  const { config, setConfig } = useContext(ScalprumContext);
+  const [scope, setScope] = useState('advisor');
+  const path = `apps/${scope}`;
+  const module = './RulesTableWrapped';
+  const manifestLocation = `/${path}/fed-mods.json`;
+
+  const [value, setValue] = useState(scope);
+
+  useEffect(() => {
+    setConfig({
+      [scope]: {
+        name: scope,
+        manifestLocation,
+        cdnPath: `/${path}/`,
+      },
+    });
+  }, [setConfig, scope, path, manifestLocation]);
+
+  return (
+    <>
+      <span>
+        <TextInput
+          value={value}
+          type="text"
+          onChange={(_event, val) => setValue(val)}
+          aria-label="manifest location"
+        />
+        <Button variant="primary" onClick={() => setScope(value)}>
+          Set scope
+        </Button>
+      </span>
+      <pre style={{ paddingBottom: '2em' }}>{manifestLocation}</pre>
+      {config[scope] && (
+        <ScalprumComponent scope={scope} module={module} {...props} />
+      )}
+    </>
+  );
+};
+
+const LocalAdvisorPlaceholderWrapped = () => (
+  <ScalprumContextWrapper>
+    <LocalAdvisorPlaceholder IopRemediationModal={RemediationModal} />
+  </ScalprumContextWrapper>
+);
+
+const RecommendationsPage = props => {
+  // TODO: remove next line before merging
+  return <LocalAdvisorPlaceholderWrapped />;
+
+  // eslint-disable-next-line no-unreachable
+  const isLocalAdvisorEngine = useAdvisorEngineConfig();
+
+  return isLocalAdvisorEngine ? (
+    <LocalAdvisorPlaceholder />
+  ) : (
+    <InsightsCloudSync {...props} />
+  );
+};
+
+export default RecommendationsPage;
