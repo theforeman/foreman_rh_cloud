5a4ae232adecb9b94cc82ec061738ce51c8e1ea4
Jeremy Lenz <jlenz@redhat.com>
2025-10-09 13:48:43 -0400

    fix tests

--- webpack/InsightsCloudSync/Components/InsightsTable/__tests__/InsightsTable.test.js
+++ webpack/InsightsCloudSync/Components/InsightsTable/__tests__/InsightsTable.test.js
@@ -3,6 +3,19 @@ import { testComponentSnapshotsWithFixtures } from '@theforeman/test';
 import InsightsTable from '../InsightsTable';
 import { tableProps } from './fixtures';
 
+jest.mock('foremanReact/Root/Context/ForemanContext', () => ({
+  useForemanContext: () => ({
+    metadata: {
+      foreman_rh_cloud: {
+        iop: true,
+      },
+    },
+  }),
+  useForemanSettings: () => ({
+    perPage: 20,
+  }),
+}));
+
 const fixtures = {
   'render with Props': tableProps,
 };
