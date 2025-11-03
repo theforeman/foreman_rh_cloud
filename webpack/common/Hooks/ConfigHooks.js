3992ced76392571924fa4bf56c5d074f40b71b01
Jeremy Lenz <jlenz@redhat.com>
2025-11-03 08:00:32 -0500

    Register foreman_rh_cloud app metadata and rename advisor engine references (#1116)
    * Register foreman_rh_cloud app metadata
    Rename advisor engine references to useIopConfig

--- webpack/common/Hooks/ConfigHooks.js
+++ webpack/common/Hooks/ConfigHooks.js
@@ -1,18 +1,5 @@
-import { useAPI } from 'foremanReact/common/hooks/API/APIHooks';
-import {
-  ADVISOR_ENGINE_CONFIG_KEY,
-  ADVISOR_ENGINE_CONFIG_PATH,
-} from '../../InsightsCloudSync/Components/InsightsTable/InsightsTableConstants';
+import { useForemanContext } from 'foremanReact/Root/Context/ForemanContext';
 
-export const useAdvisorEngineConfig = () => {
-  const { response: advisorEngineConfig } = useAPI(
-    'get',
-    ADVISOR_ENGINE_CONFIG_PATH,
-    {
-      key: ADVISOR_ENGINE_CONFIG_KEY,
-    }
-  );
-
+export const useIopConfig = () =>
   // eslint-disable-next-line camelcase
-  return advisorEngineConfig?.use_iop_mode;
-};
+  useForemanContext().metadata?.foreman_rh_cloud?.iop;
