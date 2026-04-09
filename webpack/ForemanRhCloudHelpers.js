a3348a7ee45d9948136849079e1bad9b17e8c9ee
Nofar <nofaralfasi@gmail.com>
2026-04-09 19:57:17 +0300

    Fix blank screen when switching to host without Insights tabs (#1183)
    * Fix blank screen when switching to host without Insights tabs
    
    When using breadcrumb switcher to navigate from a registered host
    (with Recommendations/Vulnerabilities tabs) to an unregistered host,
    redirect to Overview tab instead of showing a loading spinner.
    
    (cherry picked from commit 02c53f237a52930b02a2b81c2b245fc036ee85ec)

--- webpack/ForemanRhCloudHelpers.js
+++ webpack/ForemanRhCloudHelpers.js
@@ -1,9 +1,31 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 /**
  * copied from core, since it's not in the ReactApp folder,
  * it's complicated to import it and mock it in tests.
  * should be imported once core moves it to the ReactApp folder.
  */
 export const foremanUrl = path => `${window.URL_PREFIX}${path}`;
+
+export const OVERVIEW_TAB_PATH = '/Overview';
+
+/**
+ * Redirects to Overview tab when the current tab should be hidden
+ * @param {boolean} shouldRedirect - Whether to redirect (e.g., host loaded AND tab should hide)
+ * @returns {boolean} - Returns shouldRedirect for convenience
+ */
+export const useTabRedirect = shouldRedirect => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (shouldRedirect && history) {
+      history.replace(OVERVIEW_TAB_PATH);
+    }
+  }, [shouldRedirect, history]);
+
+  return shouldRedirect;
+};
 
 export const isNotRhelHost = ({ hostDetails }) =>
   // This regex tries matches sane variations of "RedHat", "RHEL" and "RHCOS"
