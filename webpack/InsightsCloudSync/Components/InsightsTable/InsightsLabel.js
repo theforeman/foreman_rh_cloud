69e39f90c7ddca96ffdd99fb4226a85eeac64b02
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-07-22 05:47:57 -0400

    SAT-32064 - update InsightsHostDetailsTab from pf3 to pf5

--- webpack/InsightsCloudSync/Components/InsightsTable/InsightsLabel.js
+++ webpack/InsightsCloudSync/Components/InsightsTable/InsightsLabel.js
@@ -7,21 +7,22 @@ import {
 } from '@patternfly/react-icons';
 import { Label } from '@patternfly/react-core';
 import PropTypes from 'prop-types';
+import { translate as __ } from 'foremanReact/common/I18n';
 
 const VALUE_TO_STATE = {
-  1: { icon: <AngleDoubleDownIcon />, text: 'Low', color: 'blue' },
-  2: { icon: <EqualsIcon />, text: 'Moderate', color: 'yellow' },
-  3: { icon: <AngleDoubleUpIcon />, text: 'Important', color: 'orange' },
-  4: { icon: <CriticalRiskIcon />, text: 'Critical', color: 'red' },
+  1: { icon: <AngleDoubleDownIcon />, text: __('Low'), color: 'blue' },
+  2: { icon: <EqualsIcon />, text: __('Moderate'), color: 'yellow' },
+  3: { icon: <AngleDoubleUpIcon />, text: __('Important'), color: 'orange' },
+  4: { icon: <CriticalRiskIcon />, text: __('Critical'), color: 'red' },
 };
 
 const InsightsLabel = ({ value = 1, text, hideIcon, ...props }) => (
   <Label
     {...props}
-    color={VALUE_TO_STATE[value].color}
-    icon={!hideIcon && VALUE_TO_STATE[value].icon}
+    color={VALUE_TO_STATE[value]?.color}
+    icon={!hideIcon && VALUE_TO_STATE[value]?.icon}
   >
-    {text || VALUE_TO_STATE[value].text}
+    {text || VALUE_TO_STATE[value]?.text}
   </Label>
 );
 
--- webpack/InsightsHostDetailsTab/InsightsTab.js
+++ webpack/InsightsHostDetailsTab/InsightsTab.js
@@ -1,56 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { orderBy } from 'lodash';
-import { Grid, ListView } from 'patternfly-react';
+import { Accordion, Grid, GridItem, Title } from '@patternfly/react-core';
 import { noop } from 'foremanReact/common/helpers';
 import { translate as __ } from 'foremanReact/common/I18n';
 import ListItem from './components/ListItem';
 import './InsightsTab.scss';
 
-class InsightsHostDetailsTab extends React.Component {
-  componentDidMount() {
-    const { fetchHits, hostID } = this.props;
+const InsightsHostDetailsTab = ({ fetchHits, hostID, hits }) => {
+  useEffect(() => {
     fetchHits(hostID);
-  }
+  }, [hostID]);
 
-  render() {
-    const { hits } = this.props;
-
-    if (!hits.length) {
-      return <h2>{__('No recommendations were found for this host!')}</h2>;
-    }
-    const hitsSorted = orderBy(hits, ['total_risk'], ['desc']);
-    const items = hitsSorted.map(
-      (
-        {
-          title,
-          total_risk: totalRisk,
-          results_url: resultsUrl,
-          solution_url: solutionUrl,
-        },
-        index
-      ) => (
-        <ListItem
-          key={index}
-          title={title}
-          totalRisk={totalRisk}
-          resultsUrl={resultsUrl}
-          solutionUrl={solutionUrl}
-        />
-      )
-    );
+  if (!hits.length) {
     return (
       <div id="host_details_insights_tab">
-        <Grid.Row>
-          <Grid.Col xs={12}>
-            <h2>{__('Recommendations')}</h2>
-            <ListView id="hits_list">{items}</ListView>
-          </Grid.Col>
-        </Grid.Row>
+        <Title
+          headingLevel="h2"
+          size="md"
+          ouiaId="insights-recommendations-empty-title"
+        >
+          {__('No recommendations were found for this host!')}
+        </Title>
       </div>
     );
   }
-}
+
+  const hitsSorted = orderBy(hits, ['total_risk'], ['desc']);
+
+  return (
+    <div id="host_details_insights_tab">
+      <Grid hasGutter>
+        <GridItem span={12}>
+          <Title
+            headingLevel="h2"
+            size="md"
+            ouiaId="insights-recommendations-title"
+          >
+            {__('Recommendations')}
+          </Title>
+          <div id="hits_list" data-testid="hits-list">
+            <Accordion>
+              {hitsSorted.map(
+                ({
+                  id,
+                  title,
+                  total_risk: totalRisk,
+                  results_url: resultsUrl,
+                  solution_url: solutionUrl,
+                }) => (
+                  <ListItem
+                    key={id}
+                    title={title}
+                    totalRisk={totalRisk}
+                    resultsUrl={resultsUrl}
+                    solutionUrl={solutionUrl}
+                  />
+                )
+              )}
+            </Accordion>
+          </div>
+        </GridItem>
+      </Grid>
+    </div>
+  );
+};
 
 InsightsHostDetailsTab.propTypes = {
   hostID: PropTypes.number.isRequired,
