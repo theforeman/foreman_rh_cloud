8d2f4e424e6293bbc1d4f4d52ee2b92a3cebf4dd
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-08-19 06:58:24 -0400

    SAT-48347 - add test_setup that imports foremanJSTestSetup and remove overlapping mocks

--- webpack/__mocks__/foremanReact/Root/Context/ForemanContext.js
+++
@@ -1,6 +0,0 @@
-export const useForemanSettings = jest.fn(() => ({ perPage: 20 }));
-export const useForemanOrganization = jest.fn(() => ({
-  id: 1,
-  title: 'some-org',
-}));
-export const useForemanContext = jest.fn(() => ({ metadata: {} }));
