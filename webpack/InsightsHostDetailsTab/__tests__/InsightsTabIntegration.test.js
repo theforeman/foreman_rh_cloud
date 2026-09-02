69b0e9562c10cac72542cf492b3ed11af7e27695
tlabaj <tlabaj@redhat.com>
2026-09-02 09:24:28 -0400

    SAT-48787 - replace InsightsTab Enzyme integration test with RTL

--- webpack/InsightsHostDetailsTab/__tests__/InsightsTabIntegration.test.js
+++ webpack/InsightsHostDetailsTab/__tests__/InsightsTabIntegration.test.js
@@ -1,17 +1,78 @@
 import React from 'react';
-import { IntegrationTestHelper } from '@theforeman/test';
+import { screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { rtlHelpers } from 'foremanReact/common/rtlTestHelpers';
+import { API } from 'foremanReact/redux/API';
 
 import InsightsTab from '../index';
 import reducers from '../../ForemanRhCloudReducers';
-import { hostID } from './InsightsTab.fixtures';
+import { INSIGHTS_HITS_SUCCESS } from '../InsightsTabConstants';
+import { hostID, hits } from './InsightsTab.fixtures';
+
+const { renderWithI18n } = rtlHelpers;
+
+jest.mock('foremanReact/redux/API');
+
+const recommendationTitle =
+  'New Ansible Engine packages are inaccessible when dedicated Ansible repo is not enabled';
+
+const createTabStore = () =>
+  createStore(combineReducers({ ...reducers }), applyMiddleware(thunk));
+
+const renderConnectedTab = (store = createTabStore()) =>
+  renderWithI18n(
+    <Provider store={store}>
+      <InsightsTab hostID={hostID} />
+    </Provider>
+  );
 
 describe('InsightsTab integration test', () => {
-  it('should flow', async () => {
-    const integrationTestHelper = new IntegrationTestHelper(reducers);
-    const component = integrationTestHelper.mount(
-      <InsightsTab hostID={hostID} />
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('requests hits for the host and shows empty state when there are none', async () => {
+    let resolveHits;
+    API.get.mockReturnValue(
+      new Promise(resolve => {
+        resolveHits = resolve;
+      })
     );
-    component.update();
-    /** Create a Flow test */
+
+    const store = createTabStore();
+    store.dispatch({
+      type: INSIGHTS_HITS_SUCCESS,
+      payload: { hits },
+    });
+
+    renderConnectedTab(store);
+
+    expect(await screen.findByText(recommendationTitle)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(API.get).toHaveBeenCalledWith(`/insights_cloud/hits/${hostID}`);
+    });
+
+    resolveHits({ data: { hits: [] } });
+
+    expect(
+      await screen.findByRole('heading', {
+        name: 'No recommendations were found for this host!',
+      })
+    ).toBeInTheDocument();
+    expect(screen.queryByText(recommendationTitle)).not.toBeInTheDocument();
+  });
+
+  it('shows recommendation titles from the hits API response', async () => {
+    API.get.mockResolvedValue({ data: { hits } });
+
+    renderConnectedTab();
+
+    expect(await screen.findByText(recommendationTitle)).toBeInTheDocument();
+
+    expect(API.get).toHaveBeenCalledWith(`/insights_cloud/hits/${hostID}`);
   });
 });
